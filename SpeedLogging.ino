b1f05da2e75e47a631a11ff7653b47998c32d86a
Josh Oldenburg <josh.oldenburg@me.com>
2019-01-18 13:21:30 -0500

    Tidy up speed logging code

--- SpeedLogging.ino
+++ SpeedLogging.ino
@@ -1,57 +1,63 @@
-
 volatile unsigned long prevSampleTime = micros();
 volatile unsigned long currSampleTime = micros();
 
 volatile unsigned int interruptCount = 0;
 volatile unsigned int prevInterruptCount = 0;
 
+double sampleTime = 0.0;
 double measuredSpeed = 0.0;
-const byte speedSensor = 2;
+
+#define USECTOSEC 1000000
+
+// Multiplied by the final speed in Hz before being output
+#define CONVERSIONFACTOR 2.0
+const byte speedSensorPin = 2;
+
 void calculateSpeed();
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(speedSensor, INPUT);
-  attachInterrupt(digitalPinToInterrupt(speedSensor), measureSpeed, FALLING);
+    pinMode(speedSensorPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(speedSensorPin), measureSpeed, FALLING);
 
-  Serial.begin(9600);
-
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  calculateSpeed();
-  Serial.print(millis()/1000.0);
-  Serial.print(",");
-  Serial.println(measuredSpeed);
-  delay(200);
+    calculateSpeed();
+
+    Serial.print(sampleTime);
+    Serial.print(",");
+    Serial.println(measuredSpeed);
+
+    delay(100);
 }
 
-void measureSpeed(){
-  currSampleTime = micros();  
-  interruptCount++;
+void measureSpeed() {
+    currSampleTime = micros();
+    interruptCount++;
 }
 
+void calculateSpeed() {
+    const unsigned long currTime = currSampleTime;
+    const unsigned long prevTime = prevSampleTime;
+    const unsigned long currInterruptCount = interruptCount;
+    if (currTime != prevTime){
+        /*Serial.print(currTime);
+        Serial.print("   ");
+        Serial.println(prevTime);
+        Serial.print(interruptCount);
+        Serial.print("---");
+        Serial.println(prevInterruptCount);*/
 
-void calculateSpeed(){
-  const unsigned long currTime = currSampleTime;
-  const unsigned long prevTime = prevSampleTime;
-  if(currTime != prevTime){
-//    Serial.print(currTime);
-//    Serial.print("   ");
-//    Serial.println(prevTime);
-//    Serial.print(interruptCount);
-//    Serial.print("---");
-//    Serial.println(prevInterruptCount);
-    measuredSpeed = (double)(interruptCount-prevInterruptCount)/((double)(currTime - prevTime)/500000.0);    
+        // Divide by 1,000,000 to convert microseconds to seconds, multiply by our conversion factor
+        double timeDelta = (double)(currTime - prevTime) / (double)(USECTOSEC / CONVERSIONFACTOR);
+        measuredSpeed = (double)(currInterruptCount - prevInterruptCount) / timeDelta;
+        sampleTime = currTime / USECTOSEC;
 
-    //Serial.println(currTime - prevTime);
-    prevSampleTime = currSampleTime;
-    prevInterruptCount = interruptCount;
-    
-  }else{
-    measuredSpeed = 0;
-    interruptCount = 0;
-  }
-  
+        prevSampleTime = currSampleTime;
+        prevInterruptCount = interruptCount;
+    } else {
+        measuredSpeed = 0;
+        interruptCount = 0;
+    }
 }
